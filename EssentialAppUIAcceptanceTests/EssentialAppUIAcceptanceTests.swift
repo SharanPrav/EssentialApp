f3a134804c100dffe1b7addfdc7f4be243767306
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-02-21 22:57:34 +0100

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -5,14 +5,14 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedsWhenThereIsConnectivity() {
         let app = XCUIApplication()
         
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         app.launch()
         
-        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.count, 2)
         XCTAssertEqual(app.cells.firstMatch.images.count, 1)
 
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
@@ -20,7 +20,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         onlineApp.launch()
         
         let offlineApp = XCUIApplication()
@@ -28,7 +28,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         offlineApp.launch()
         
         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCells.count, 22)
+        XCTAssertEqual(cachedFeedCells.count, 2)
         
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
