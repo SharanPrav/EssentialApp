65da96ab2159f15e6be82332c771a1499f874503
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-02-21 16:45:14 +0100

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -10,24 +10,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+                        
+        //"https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         
         let session = URLSession(configuration: .ephemeral)
         let client = URLSessionHTTPClient(session: session)
-        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: client)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: client)
-        
-        let localStroreURL = NSPersistentContainer
-            .defaultDirectoryURL
-            .appendingPathComponent("feed-store.sqlite")
-        
-        let localDataStore = try! CoreDataFeedStore(storeURL: localStroreURL)
-        let localfeedLoader = LocalFeedLoader(store: localDataStore, currentDate: Date.init)
-        let localImageLoader = LocalFeedImageDataLoader(store: localDataStore)        
-        
-        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localfeedLoader), fallback: localfeedLoader), imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader, cache: localImageLoader)))
-        
-        window?.rootViewController = feedViewController
+
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+                    feedLoader: remoteFeedLoader,
+                    imageLoader: remoteImageLoader)
     }
 }
 
--- EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -2,4 +2,17 @@ import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
 
+    func test_onLaunch_displaysRemoteFeedsWhenThereIsConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 22)
+        
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
+    }
 }
