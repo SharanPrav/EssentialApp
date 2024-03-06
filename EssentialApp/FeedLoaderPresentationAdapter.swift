fea1c2bd21174f68a75fcf51431e41c8079d9ba2
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-03-06 16:04:39 +0100

    Replace FeedPresenter with generic presenter

---
+++ EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,32 @@
+import Foundation
+import EssentialFeed
+import EssentialFeediOS
+import Combine
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
+    private var cancellable: Cancellable?
+    var presenter: LoadResourcePresenter<[FeedImage], FeedViewAdapter>?
+    
+    init(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoading()
+        
+        cancellable = feedLoader()
+            .dispatchOnMainQueue()
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                        
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoading(with: error)
+                    }
+                }, receiveValue: { [weak self] feed in
+                    self?.presenter?.didFinishLoading(with: feed)
+                })
+    }
+}
--- EssentialApp/FeedUIComposer.swift
+++ EssentialApp/FeedUIComposer.swift
@@ -7,21 +7,18 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-        //        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
-        //        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
 
         let feedController = makeFeedViewController(
             delegate: presentationAdapter,
             title: FeedPresenter.title)
         
-        presentationAdapter.presenter = FeedPresenter(
-            feedView: FeedViewAdapter(
-                controller: feedController,
-                imageLoader: imageLoader),
-            loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController))
-        
-        //        feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+        presentationAdapter.presenter = LoadResourcePresenter(resourceView: FeedViewAdapter(
+            controller: feedController,
+            imageLoader: imageLoader), 
+                                                              loadingView: WeakRefVirtualProxy(feedController), 
+                                                              errorView: WeakRefVirtualProxy(feedController),
+                                                              mapper: FeedPresenter.map)
         return feedController
     }
     
@@ -32,33 +29,5 @@ public final class FeedUIComposer {
         feedController.delegate = delegate
         feedController.title = title
         return feedController
-    }
-}
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
-    private var cancellable: Cancellable?
-    var presenter: FeedPresenter?
-    
-    init(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) {
-        self.feedLoader = feedLoader
-    }
-    
-    func didRequestFeedRefresh() {
-        presenter?.didStartLoadingFeed()
-        
-        cancellable = feedLoader()
-            .dispatchOnMainQueue()
-            .sink(
-                receiveCompletion: { [weak self] completion in
-                    switch completion {
-                    case .finished: break
-                        
-                    case let .failure(error):
-                        self?.presenter?.didFinishLoadingFeed(with: error)
-                    }
-                }, receiveValue: { [weak self] feed in
-                    self?.presenter?.didFinishLoadingFeed(with: feed)
-                })
     }
 }
