2f628eff48aae052fa45c9801350194e7ed13f70
Saranya Ravi <saranya.ravi@nederlandselotterij.nl>
2024-04-05 19:22:39 +0200

    Show loading more indicator while loading more

--- EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -90,8 +90,8 @@ extension ListViewController {
     }
     
     func simulateLoadMoreFeedAction() {
-        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
-        
+        guard let view = loadMoreFeedCell() else { return }
+
         let delegate = tableView.delegate
         let index = IndexPath(row: 0, section: feedLoadMoreSection)
         delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
@@ -115,6 +115,14 @@ extension ListViewController {
         let ds = tableView.prefetchDataSource
         let index = IndexPath(row: row, section: feedImagesSection)
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+    
+    var isShowingLoadMoreFeedIndicator: Bool {
+        return loadMoreFeedCell()?.isLoading == true
+    }
+
+    private func loadMoreFeedCell() -> LoadMoreCell? {
+        cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
 
     func renderedFeedImageData(at index: Int) -> Data? {
